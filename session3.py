79f640b577c5cc07de53275f7185c54d9624c493
Ritesh_Gupta <59221074+riteshgupta0406@users.noreply.github.com>
2020-12-21 00:12:13 +0530

    Added json file storage

--- session3.py
+++ session3.py
@@ -1,31 +1,37 @@
+import json
+
+user_file = 'c:/Users/RITZ/Documents/Python/user_list.json'
+with open(user_file) as usrs:
+        usr_list = json.load(usrs)
+
 def log_in(a,b):
-    if a in users:
-        if users[a] == b:
+        if usr_list[a] == b:
             print ("welcome! aaha login")
         else:
-            print ("Chala ja bsdk")
-            exit()
-    else:
-        print("username does not exist")
-        exit()
+            print ("Password galat h")
 
 def sign_up(a,b):
-    if a in users:
-        print("Doosra naam dhoondh bisi")
-    else:
-        users[a] = b
+    usr_list[a] = b
+    with open(user_file,'w') as usrs:
+        json.dump(usr_list,usrs)
+
 
 user_inp = int(input("1. Log In\n2. Sign Up\n"))
-users = {"usr_1":"pass_1","usr_2":"pass_2","usr_3":"pass_3"}
 if user_inp == 1:
     print ("Yaha login karein")
     a = str(input("username: "))
-    b = str(input("pwd: "))
-    log_in(a,b)
+    if(a in usr_list):
+        b = str(input("pwd: "))
+        log_in(a,b)
+    else:
+        print("Username not registered. Please try again!!")
 elif  user_inp == 2:
     a = str(input("username: "))
-    b = str(input("pwd: "))
-    sign_up(a,b)
+    if a in usr_list:
+        print("Sorry this Username is already taken. Try some other...")
+    else:
+        b = str(input("pwd: "))
+        sign_up(a,b)
 else:
     print ("bhai galti kar diye, chalo bhago")
     exit()
